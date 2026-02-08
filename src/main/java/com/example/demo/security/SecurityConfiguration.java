bd2f52c18e48a9872f8c75fce545f03cdc593b56
sukritcharredsoul <sukritchoudhary16@gmail.com>
2026-02-08 21:53:23 +0530

    Changed the SecurityConfiguration for JWT

--- src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -8,22 +8,37 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
     private final CustomerDetailService userDetailsService;
+    private final jwtAuthenticationFilter jwtAuthenticationFilter ;
 
-    public SecurityConfiguration(CustomerDetailService userDetailsService) {
+    public SecurityConfiguration(CustomerDetailService userDetailsService, jwtAuthenticationFilter jwtAuthenticationFilter) {
         this.userDetailsService = userDetailsService;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter ;
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll().anyRequest().authenticated()) ;
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**")
+                        .permitAll()
+                        .anyRequest().authenticated())
+
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) ;
+
         return http.build() ;
     }
 
